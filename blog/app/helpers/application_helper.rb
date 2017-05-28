e5a63229f4b702c9d3595dbd6f44a1a1d5e92f5e
mmusau99 <mmusau99@gmail.com>
2017-05-28 16:27:24 -0500

    creates navbar for blog

--- blog/app/helpers/application_helper.rb
+++ blog/app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    session[:user_id] != nil
+  end
+
+  def authenticate!
+    redirect '/users/login' unless logged_in?
+  end
+  def authorize?
+    if logged_in? && current_user.id == session[:user_id]
+  	end
+  end
 end
