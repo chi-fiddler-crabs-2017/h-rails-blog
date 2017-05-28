25a0eab5eeb492bca267cc32147bf414c74b2234
Brittany Baranowski <brittanybaranowski@gmail.com>
2017-05-28 15:52:12 -0500

    Create post model, add create method to posts controller along with private method to specify strong params

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -4,4 +4,17 @@ class PostsController < ApplicationController
 
   def new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+# Rails 4 secuirty feature called strong params, need to specify what params you want to allow under private method
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
