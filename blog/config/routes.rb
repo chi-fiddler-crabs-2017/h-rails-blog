8f80e18207cf314c1f4038d8a278f7b02bc7e403
Brittany Baranowski <brittanybaranowski@gmail.com>
2017-05-28 21:12:40 -0500

    Create pages controller and config routes

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,9 @@
 Blog::Application.routes.draw do
-  resources :posts do
+  resources :posts do #resources will provide all the restful routes
     resources :comments #nested route
   end
 
   root "posts#index"
+
+  get '/about', to: 'pages#about' # just have to do /about
 end
