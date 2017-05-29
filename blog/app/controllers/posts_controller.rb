b5a358b64cbb4b6e5a888f650c20b1ee76f44911
Brittany Baranowski <brittanybaranowski@gmail.com>
2017-05-28 19:23:03 -0500

    Add show page and method

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -12,6 +12,9 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
 # Rails 4 secuirty feature called strong params, need to specify what params you want to allow under private method
   private
     def post_params
