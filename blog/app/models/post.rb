b9ba7745135ca4a0c62ecf52412de8d8d354d79d
Brittany Baranowski <brittanybaranowski@gmail.com>
2017-05-28 21:24:11 -0500

    Add pages and else if for links

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments, dependent: :destroty #if post deleted then comment will be deleted
+  has_many :comments, dependent: :destroy #if post deleted then comment will be deleted
 
   validates :title, presence: true, length: { minimum: 5}
   validates :body, presence: true
