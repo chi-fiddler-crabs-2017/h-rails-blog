e23444023dbd6184fc21cd0c115315d81d033060
Brittany Baranowski <brittanybaranowski@gmail.com>
2017-05-28 21:10:44 -0500

    Create ability to add and delete comments

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroty #if post deleted then comment will be deleted
 
   validates :title, presence: true, length: { minimum: 5}
   validates :body, presence: true
